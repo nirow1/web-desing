148ea3b1cec2f0e5bea9a0e5b9c84d7730cfe08c
nirow1 <63425228+nirow1@users.noreply.github.com>
2021-11-29 22:50:24 +0100

    Add files via upload

--- značkovací jazyky/index.js
+++ značkovací jazyky/index.js
@@ -1,7 +1,12 @@
 //selectors
 let header = document.querySelector('.header');
 let hamburgerMenu = document.querySelector('.hamburger-menu');
+let searchbar = document.querySelector('.search-btn');
 
 hamburgerMenu.addEventListener('click', function(){
 	header.classList.toggle('menu-open');
+})
+
+searchbar.addEventListener('click', function(){
+	header.classList.toggle('search-open');
 })
